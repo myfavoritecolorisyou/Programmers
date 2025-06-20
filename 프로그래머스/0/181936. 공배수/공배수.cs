4cdb39e3f94fef5460f1793db8713e99d5929ea5
이은석 <eunseokleedev@gmail.com>
2025-06-20 09:51:39 +0900

    [level 0] Title: 공배수, Time: 0.24 ms, Memory: 31.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181936. 공배수/공배수.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class Solution {
+    public int solution(int number, int n, int m) {
+        int answer = 0;
+
+        if ((number >= 10 && number <= 100) && (n >= 2 && n <= 10))
+        {
+            if ((number % n == 0) && (number % m == 0)) answer = 1;
+            else answer = 0;
+        }
+
+        return answer;
+    }
+}
