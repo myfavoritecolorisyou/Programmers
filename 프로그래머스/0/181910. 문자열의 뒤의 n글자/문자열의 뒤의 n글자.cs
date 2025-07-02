631ad8296f7fed3f9040aab33c50d94cdbb3fdfb
이은석 <eunseokleedev@gmail.com>
2025-07-02 22:10:00 +0900

    [level 0] Title: 문자열의 뒤의 n글자, Time: 0.83 ms, Memory: 31.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181910. 문자열의 뒤의 n글자/문자열의 뒤의 n글자.cs
@@ -0,0 +1,19 @@
+using System;
+
+public class Solution {
+    public string solution(string my_string, int n)
+    {
+        string answer = "";
+
+        if ((my_string.Length >= 1 && my_string.Length <= 1000)
+            && (n >= 1 && n <= my_string.Length))
+        {
+            for (int i = my_string.Length - n; i < my_string.Length; i++)
+            {
+                answer += my_string[i];
+            }
+        }
+
+        return answer;
+    }
+}
