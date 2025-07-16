17a42b5ccabc32475c144fe1cbdd3cb3094ee0ee
이은석 <eunseokleedev@gmail.com>
2025-07-16 16:24:57 +0900

    [level 0] Title: n 번째 원소부터, Time: 0.93 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181892. n 번째 원소부터/n 번째 원소부터.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+
+public class Solution {
+    public int[] solution(int[] num_list, int n) {
+        int[] answer = new int[] { };
+        var temp_list = new List<int>();
+
+        if ((num_list.Length >= 2 && num_list.Length <= 30)
+            && (n >= 1 && n <= num_list.Length))
+        {
+            for (int i = n - 1; i < num_list.Length; i++)
+            {
+                if (num_list[i] >= 1 && num_list[i] <= 9)
+                    temp_list.Add(num_list[i]);
+            }
+        }
+
+        answer = temp_list.ToArray();
+
+        return answer;
+    }
+}
